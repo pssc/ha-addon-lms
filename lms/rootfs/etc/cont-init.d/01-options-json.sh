c0f3acadc83054626e734ff165cecd8ea6c7dfd3
pssc <phillip.camp+git@gmail.com>
2020-05-04 21:31:04 +0100

    use a saved addon config if directed

---
+++ lms/rootfs/etc/cont-init.d/01-options-json.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Community Hass.io Add-ons: lms
+# Set up config from saved
+# ==============================================================================
+
+CF="/config/lms/options.json"
+CD="/data/options.json"
+CS="${CD}.orig"
+
+function main {
+  KEY="saved"
+  VAL=$(bashio::config "${KEY,,}")
+  if [[ "${VAL}" = "true" ]];then
+    if [[ -f "${CF}" ]];then
+      bashio::log.info "Using Saved config"
+      mv "${CD}" "${CS}"
+      cp "${CF}" "${CD}"
+      jq -C "" <"${CD}"
+    fi
+  fi
+}
+
+main
+
+exit
