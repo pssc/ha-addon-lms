96235f496b1533e95f2ed91a0f8f6c1032b46e23
pssc <phillip.camp+git@gmail.com>
2020-07-06 12:52:52 +0100

    Increase shutdown timeout to all cleanup and run from ram, allow for custom init scripts and packages

---
+++ lms/rootfs/etc/cont-init.d/17-packages.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Install user configured/requested packages
+# ==============================================================================
+if bashio::config.has_value 'packages'; then
+   bashio::log.info " Installing user configured/requested packages"
+   apt-get update \
+        || bashio::exit.nok 'Failed updating Alpine packages repository indexes'
+
+    for package in $(bashio::config 'packages'); do
+        apt-get install -y --no-suggests --no-recommends  "$package" \
+            || bashio::exit.nok "Failed installing package ${package}"
+    done
+fi
