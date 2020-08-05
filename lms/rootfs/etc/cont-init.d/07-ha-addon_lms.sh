2a799cda6626816fda01a55a2b58a3d5ca503e8a
pssc <phillip.camp+git@gmail.com>
2020-08-05 13:13:10 +0100

    Auto set friendley name from container name

--- lms/rootfs/etc/cont-init.d/07-ha-addon_lms.sh
+++ lms/rootfs/etc/cont-init.d/07-ha-addon_lms.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/with-contenv bash
 # ==============================================================================
 # Community Hass.io Add-ons: lms
 # Set up env from ha
 # ==============================================================================
 
-HAA_NAME="lms"
-echo -n ${HAA_NAME} >/run/s6/container_environment/HAA_NAME
+echo -n ${HAA_NAME:-$(hostname -s|sed 's/.*-//')} >/run/s6/container_environment/HAA_NAME
 
 exit 0
