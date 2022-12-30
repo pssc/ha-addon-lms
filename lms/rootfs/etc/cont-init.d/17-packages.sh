9852cf40f66deb8ebfc3e0be17e818ac99a8f625
pssc <phillip.camp+git@gmail.com>
2022-12-30 11:08:01 +0000

    Allow updates without package installs

--- lms/rootfs/etc/cont-init.d/17-packages.sh
+++ lms/rootfs/etc/cont-init.d/17-packages.sh
@@ -2,15 +2,22 @@
 # ==============================================================================
 # Install user configured/requested packages
 # ==============================================================================
-if bashio::config.has_value 'packages'; then
+if bashio::config.true 'upgrade_packages'; then
    bashio::log.info " Updating package lists"
    apt-get update \
-        || bashio::exit.nok 'Failed updating packages repository indexes'
+        || bashio::.log.error 'Failed updating packages repository indexes'
 
    if [ "$(bashio::config 'upgrade_packages')" = "true" ];then
         apt-get dist-upgrade \
             || bashio::log.error 'Failed updating packages'
    fi
+fi
+
+
+if bashio::config.has_value 'packages'; then
+   bashio::log.info " Updating package lists"
+   apt-get update \
+        || bashio::.log.error 'Failed updating packages repository indexes'
 
    for package in $(bashio::config 'packages'); do
         bashio::log.info " Installing package ${package}"
