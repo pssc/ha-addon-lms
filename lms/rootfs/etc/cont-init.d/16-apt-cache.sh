ab58b2fc6dcd71510ab2822d40c292571985489c
pssc <phllip.camp+git@gmail.com>
2024-03-05 16:17:16 +0000

    Use package cache if present

---
+++ lms/rootfs/etc/cont-init.d/16-apt-cache.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/with-contenv bashio
+# ==============================================================================
+# Install user configured/requested packages
+# ==============================================================================
+
+#if bashio::config.true 'upgrade_packages'; then
+#if bashio::config.true 'cache_packages'; then
+#if bashio::config.has_value 'packages'; then
+
+if [ -r "${LMS_HACFGDIR}/apt" ];then
+        bashio::log.info " Using addon cachedir ${LMS_HACFGDIR}/apt for /var/cache/apt"
+	rm -rf /var/cache/apt
+	ln -s "${LMS_HACFGDIR}/apt" /var/cache/apt
+	apt-get autoclean || true
+fi
+
+exit 0
