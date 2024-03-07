67e13ff3d6a6b56410c9ba4b483f5a60cb131f25
pssc <phllip.camp+git@gmail.com>
2024-03-07 14:57:08 +0000

    remove docker clean to keep packages

--- lms/rootfs/etc/cont-init.d/16-apt-cache.sh
+++ lms/rootfs/etc/cont-init.d/16-apt-cache.sh
@@ -9,6 +9,7 @@
 
 if [ -r "${LMS_HACFGDIR}/apt" ];then
         bashio::log.info " Using addon cachedir ${LMS_HACFGDIR}/apt for /var/cache/apt"
+        rm /etc/apt/apt.conf.d/docker-clean
 	cat >/etc/apt/apt.conf.d/keep <<EOF
 APT::Keep-Downloaded-Packages "true";
 EOF
