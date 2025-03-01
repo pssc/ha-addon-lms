1f5ad9132c3dc26fe95f36dfdb89ad45c4d7e897
pssc <phillip.camp+git@gmail.com>
2025-03-01 17:28:36 +0000

    add more logging information around migration

--- lms/rootfs/etc/cont-init.d/90-lms_paths_perms.sh
+++ lms/rootfs/etc/cont-init.d/90-lms_paths_perms.sh
@@ -17,21 +17,26 @@ if [ -x "${LMS_OCFG}" ]; then
    if [ "${LMS_migrate_config:-""}" = "true" ];then
         bashio::log.info " Config migration"
 	if [ -d "${LMS_OCFG}" -a -l "${LMS_CFG}" ];then
+                bashio::log.info " Trying Config migration"
 		# Delete Link
 		rm -v "${LMS_CFG}"
 		# Move dir and error delete copy and abort
 		mv -v "${LMS_OCFG}" "${LMS_CFG}"
 		if [ $? -gt 0 ];then
-                        bashio::error.info " Migration Failed"
+                        bashio::error.info " Migration Failed error during mv command tidying up..."
 			rm -rvf "${LMS_CFG}"
 			exit 1
 		fi
+	else
+           bashio::log.warn " Config migration skiped source not dir or dest not link"
 	fi
    else
      # not migrating
      if [ ! -x "${LMS_CFG}" ] ;then
-        bashio::log.info " Configiuring to use old config "
+        bashio::log.info " Configiuring to use old config inlinking to ${LMS_OCFG}"
         ln -s ${LMS_OCFG} ${LMS_CFG}
+     else
+        bashio::log.info " /config/lms exists"
      fi
    fi
 fi
